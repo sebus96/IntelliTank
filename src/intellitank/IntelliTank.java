ee46c63a8352757a84e58b70b30b1b0a9f5e682f
sebi <sebid.96@gmail.com>
2017-11-19 18:00:47 +0100

    changed title

--- src/intellitank/IntelliTank.java
+++ src/intellitank/IntelliTank.java
@@ -45,7 +45,7 @@ public class IntelliTank extends Application {
      
         gsc = new GasStationController();
         //Create the foundation: borderPane -> Scrollpane -> Canvas
-        primaryStage.setTitle("Bla bla blubb");
+        primaryStage.setTitle("Bla bla bli bla blu");
         BorderPane border = new BorderPane();
         ScrollPane sp = new ScrollPane();
         border.setCenter(sp);
